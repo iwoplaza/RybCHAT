5582d41483d41cf4df0ea3c5451905aac0a559c6
Iwo Plaza <gobbobpl@gmail.com>
2018-02-26 16:59:24 +0100

    Eh?

--- server/WebHandler.js
+++ server/WebHandler.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var expressApp = express();
 var HTTPServer = require('http').Server(expressApp);
-var io = require('socket.io')(HTTPServer);
+var io = require('socket.io');
 
 module.exports = {
     startServer: function(callback) {
